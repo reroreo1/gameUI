e23e80c4a1500ebd67bf8056f034eb0f031fb9d9
Reroreo1 <86200459+reroreo1@users.noreply.github.com>
2023-08-15 03:26:15 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,71 +1,86 @@
 const rightPaddle = document.getElementById("right-paddle");
-// console.log("right paddle ====", rightPaddle);
 const leftPaddle = document.getElementById("left-paddle");
-// console.log("right paddle ====", leftPaddle);
-const canvasElement = document.getElementById("canva1");
-// console.log("canva === ",canvasElement);
-const canvasHeightString = getComputedStyle(canvasElement).height;
-const canvasHeight = parseInt(canvasHeightString, 10);
-console.log("canvasHeight === ", canvasHeight);
-const computedStyle = getComputedStyle(leftPaddle);
-const paddleHeightString = computedStyle.height;
-const paddleHeight = parseInt(paddleHeightString, 10);
-console.log("paddleHeight === ", paddleHeight);
-function movePaddle(event) {
-    const keyCode = event.keyCode;
+const scoreLeft = document.getElementById("score-left");
+const scoreRight = document.getElementById("score-right");
+const ballElement = document.getElementById("ball1");
+const paddleSpeed = 2;
+const initPaddlePos = 50;
 
-    console.log(rightPaddle.style.top);
-    // Move left paddle
-    if (keyCode === 38) { // Up arrow
-      if()   
-        rightPaddle.style.top = (parseInt(rightPaddle.style.top) - 10).toString() +"px";
-    } else if (keyCode === 40) { // Down arrow
-        rightPaddle.style.top = (parseInt(rightPaddle.style.top) + 10).toString() +"px";
+let gameData = {
+    players: {
+        left: {
+            y: initPaddlePos,
+            score: 0
+        },
+        right: {
+            y: initPaddlePos,
+            score: 0
+        }
+    },
+    ball: {
+        ballY: 50,
+        ballX: 50,
     }
-    // leftPaddleY = Math.max(0, Math.min(leftPaddleY, canvas.height - paddleHeight));
-    rightPaddle.style.top = (Math.max(0, Math.min(rightPaddle.style.top, canvasHeight - paddleHeight))).toString() + "px";
 }
-document.addEventListener("keydown",movePaddle,false);
+let ballSpeedX =  1;
+let ballSpeedY =  5;
+let keysArr = {
+    w: false,
+    s: false,
+    up: false,
+    down: false
+};
 
-let ball = document.getElementById("ball");
-// let scoreLeft = 0; //score for player one
-// let scoreRight = 0; //score for player two
-let scoreLeft = document.getElementById("score-left");
-let scoreRight = document.getElementById("score-right");
 
-// let ballX = 0;
-// let ballY = 0;
-// let ballSpeedX = 5;
-// let ballSpeedY = 5;
+function keysHandler(event) {
+    let isPressed = event.type == "keydown";
+    switch (event.code) {
+        case "KeyS":
+            keysArr.s = isPressed;
+            break;
+        case "KeyW":
+            keysArr.w = isPressed;
+            break;
+        case "ArrowUp":
+            keysArr.up = isPressed;
+            break;
+        case "ArrowDown":
+            keysArr.down = isPressed;
+            break;
+    }
+}
+document.addEventListener("keydown", keysHandler, false);
+document.addEventListener("keyup", keysHandler, false);
 
-// let paddleSpeed = 10;
+let gameLoop = setInterval(updateUI, 1000 / 60);
+let paddlesup = setInterval(updateGameData, 1000 / 128);
 
-// let gameLoop = setInterval(update, 1000 / 60);
 
-// function update() {
-//     // console.log("update");
-//     // console.log(ballX, ballY);
-//     ballX += ballSpeedX;
-//     ballY += ballSpeedY;
-    
-//     if (ballX > 1000) {
-//         ballSpeedX *= -1;
-//         scoreLeft++;
-//         scoreLeft.innerHTML = scoreLeft;
-//     }
-//     if (ballX < 0) {
-//         ballSpeedX *= -1;
-//         scoreRight++;
-//         scoreRight.innerHTML = scoreRight;
-//     }
-//     if (ballY > 500) {
-//         ballSpeedY *= -1;
-//     }
-//     if (ballY < 0) {
-//         ballSpeedY *= -1;
-//     }
-    
-//     ball.style.left = ballX + "px";
-//     ball.style.top = ballY + "px";
-// }
+function updateGameData() {
+    if (gameData.ball.ballX + ballSpeedX <= 100 && gameData.ball.ballX - ballSpeedX >= 0) 
+        gameData.ball.ballX -= ballSpeedX;
+    // gameData.ball.ballY += ballSpeedY;
+    if (keysArr.down) { // Up arrow
+        if (gameData.players.left.y + paddleSpeed <= 100)
+            gameData.players.left.y += paddleSpeed;
+    }
+    if (keysArr.up) { // Down arrow
+        if (gameData.players.left.y - paddleSpeed >= 0)
+            gameData.players.left.y -= paddleSpeed;
+    }
+    if (keysArr.s) { // Up arrow
+        if (gameData.players.right.y + paddleSpeed <= 100)
+            gameData.players.right.y += paddleSpeed;
+    }
+    if (keysArr.w) { // Down arrow
+        if (gameData.players.right.y - paddleSpeed >= 0)
+            gameData.players.right.y -= paddleSpeed;
+    }
 
+}
+
+function updateUI() {
+    rightPaddle.style.transform = `translateY(${(gameData.players.right.y - 50) * 4}%)`;
+    leftPaddle.style.transform = `translateY(${(gameData.players.left.y - 50) * 4}%)`;
+    ballElement.style.transform = `translateX(${(gameData.ball.ballX - 50) * 2967.48 / 100 }%)`;
+}
